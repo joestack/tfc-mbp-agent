bdfdffd49702d73f4b36b13f624bbd59ac73a3bb
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-11-24 13:31:36 +0100

    21

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
 provider "kubernetes" {
-  config_path = "/root/.kube/config"
-#  load_config_file = "false"
-
-#  host = "https://kubernetes.docker.internal:6443"
-  #insecure = "true"
-
- # client_certificate     = "${var.client_certificate}"
- # client_key             = "${var.client_key}"
- # cluster_ca_certificate = "${var.cluster_ca_certificate}"
-
+  #config_path = "/root/.kube/config"
 }
 
 resource "kubernetes_namespace" "example" {
