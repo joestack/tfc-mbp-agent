686ced83ebfbd6837a0f3d734ce03a5d644307be
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-11-24 12:47:03 +0100

    12

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "kubernetes" {
  insecure = "true"
 # token = var.k8s_token
 
- client_certificate     = "var.client_certificate"
- client_key             = "var.client_key"
- cluster_ca_certificate = "var.cluster_ca_certificate"
+ client_certificate     = var.client_certificate
+ client_key             = var.client_key
+ cluster_ca_certificate = var.cluster_ca_certificate
 
 }
 
