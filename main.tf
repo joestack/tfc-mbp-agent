6606701e34b62b77e6f24fa8cba989a88349a870
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-11-24 13:06:21 +0100

    13

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 provider "kubernetes" {
   load_config_file = "false"
 
- host = "https://kubernetes.docker.internal:6443"
- insecure = "true"
-# token = var.k8s_token
+  host = "https://kubernetes.docker.internal:6443"
+  #insecure = "true"
 
- client_certificate     = var.client_certificate
- client_key             = var.client_key
- cluster_ca_certificate = var.cluster_ca_certificate
+  client_certificate     = "${var.client_certificate}"
+  client_key             = "${var.client_key}"
+  cluster_ca_certificate = "${var.cluster_ca_certificate}"
 
 }
 
