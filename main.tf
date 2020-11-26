f62a1daba5c99efc27a2263e42f57b4069d04360
Jörn Stenkamp <joern@Jorns-MacBook-Pro.local>
2020-11-26 16:55:27 +0100

    helm 1

--- main.tf
+++ main.tf
@@ -1,9 +1,27 @@
-provider "kubernetes" {
-  config_path = "/root/.kube/config"
+#provider "kubernetes" {
+#  config_path = "/root/.kube/config"
+#}
+#
+#resource "kubernetes_namespace" "example" {
+#  metadata {
+#    name = "my-first-namespace"
+#  }
+#}
+
+provider "helm" {
+  kubernetes {
+    config_path = "/root/.kube/config"
+  }
 }
 
-resource "kubernetes_namespace" "example" {
-  metadata {
-    name = "my-first-namespace"
-  }
+resource "helm_release" "vault" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  #version    = "6.0.1"
+
+  values = [
+    "${file("values.yaml")}"
+  ]
+
 }
