ac1762389064cd84b800f92d35223b7bd3eb2099
Jörn Stenkamp <joern@Jorns-MBP.fritz.box>
2020-11-27 11:33:48 +0100

    helm 4.3

--- mariadb.tf
+++ mariadb.tf
@@ -3,14 +3,4 @@ resource "helm_release" "mariadb" {
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "mariadb"
  
-
-  set {
-    name  = "rootUser.password"
-    value = "1234"
-  }
-
-  set {
-    name  = "db.user"
-    value = "testdb"
-  }
 }
